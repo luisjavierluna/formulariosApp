332ad7856560441943291c29acd25a89f1c08b8e
luisjavierluna <javierlunaluis@gmail.com>
2023-07-30 20:28:46 -0600

    4. Primeros pasos en formularios reactivos

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
@@ -14,7 +15,8 @@ import { SwitchesComponent } from './switches/switches.component';
     SwitchesComponent
   ],
   imports: [
-    CommonModule,
+    CommonModule, 
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ]
 })
