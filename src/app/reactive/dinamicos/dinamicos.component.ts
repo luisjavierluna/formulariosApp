9059a4f0ef6fa708b0b6acc4ebd54eda9b4fb772
luisjavierluna <javierlunaluis@gmail.com>
2023-08-05 20:56:33 -0600

    9. Tarea Validar un nuevo campo

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent{
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]]
+  })
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors
+      && this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if(this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    console.log(this.miFormulario.value)
   }
 
 }
