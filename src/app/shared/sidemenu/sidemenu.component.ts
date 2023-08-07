67dec8030f49d61f2bbedfc937ab3e93b6dae988
luisjavierluna <javierlunaluis@gmail.com>
2023-08-06 20:35:55 -0600

    4. Resolución de la tarea - Ruta y LazyLoad

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -48,5 +48,16 @@ export class SidemenuComponent {
     },
   ]
 
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Registro',
+      ruta: './auth/registro'
+    },
+    {
+      texto: 'Login',
+      ruta: './auth/login'
+    },
+  ]
+
 
 }
