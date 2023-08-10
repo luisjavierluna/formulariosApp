a857610a95386456e8af89b8e6a5712f5926ffc1
luisjavierluna <javierlunaluis@gmail.com>
2023-08-09 22:07:22 -0600

    7. Evaluar un email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -10,14 +10,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
   nombreApellidoPatern:string = '([a-zA-Z]+) ([a-zA-Z]+)'
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPatern) ]]
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPatern) ]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
   })
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Fernando Herrera',
+      email: 'test1@test.com',
+    })
   }
 
   campoNoValido(campo: string) {
